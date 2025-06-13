7429523d367c862e0730e0ae6e58346e4f1194eb
Pierre BULGARE <pierre.bulgare@gmail.com>
2025-06-13 17:13:38 +0200

    Fix Prod Port (8009)

--- docker_pull_run_PROD.bat
+++ docker_pull_run_PROD.bat
@@ -2,4 +2,4 @@ docker stop orange-county-lettings-PROD 2>NUL
 docker rm orange-county-lettings-PROD 2>NUL
 docker rmi pierrebulgare/orange-county-lettings:latest 2>NUL
 docker pull pierrebulgare/orange-county-lettings:latest
-docker run -d --name orange-county-lettings-PROD -p 8009:8009 pierrebulgare/orange-county-lettings:latest
+docker run -d --name orange-county-lettings-PROD -p 8009:8000 pierrebulgare/orange-county-lettings:latest
