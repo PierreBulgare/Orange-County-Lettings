c90f1a200e9208e850631cbbf18cca2803f6fe09
Pierre BULGARE <pierre.bulgare@gmail.com>
2025-06-13 17:08:58 +0200

    Corection Bat

--- docker_pull_run_DEV.bat
+++ docker_pull_run_DEV.bat
@@ -1,4 +1,5 @@
+docker stop orange-county-lettings-DEV 2>NUL
+docker rm orange-county-lettings-DEV 2>NUL
+docker rmi pierrebulgare/orange-county-lettings:dev 2>NUL
 docker pull pierrebulgare/orange-county-lettings:dev
-docker stop orange-county-lettings-DEV
-docker rm orange-county-lettings-DEV
 docker run -d --name orange-county-lettings-DEV -p 8000:8000 pierrebulgare/orange-county-lettings:dev
--- docker_pull_run_PROD.bat
+++ docker_pull_run_PROD.bat
@@ -1,4 +1,5 @@
+docker stop orange-county-lettings-PROD 2>NUL
+docker rm orange-county-lettings-PROD 2>NUL
+docker rmi pierrebulgare/orange-county-lettings:latest 2>NUL
 docker pull pierrebulgare/orange-county-lettings:latest
-docker stop orange-county-lettings-PROD
-docker rm orange-county-lettings-PROD
 docker run -d --name orange-county-lettings-PROD -p 8000:8000 pierrebulgare/orange-county-lettings:latest
